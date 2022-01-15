480441113a50d0f29a7314fb0f5edb134a298a6c
Code-it <code.it.ytb@gmail.com>
2022-01-15 11:53:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+import socket
+import getpass
+import webbrowser
 
+def openprojecturl(projectname):
+  url = "https://github.com/code-it-ytb/" + projectname + "/archive/refs/heads/main.zip"
+  webbrowser.open(url)
+
+print("Language de programmation Code It")
+
+running = True
+
+while running:
+  codeline = input(getpass.getuser() + "@" + socket.gethostname() + ":~$")
+  if "getproject " in codeline:
+    projectname = codeline.replace('getproject ', '')
+    openprojecturl(projectname)
